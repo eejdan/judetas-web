c7b69882670fb63b5b64bfea15e25824a7d9184f
eejdan <65033345+eejdan@users.noreply.github.com>
2022-04-07 01:45:33 +0300

    layout beginnings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 
+import { createContext, useEffect, useState } from 'react';
+
 import './App.css';
+// import Layout from './util/Layout';
+//import UserReq from './components/UserReq'
+import Header from './components/Header'
+
+
+import axios from 'axios';
+
+const TabsContext = createContext(null);
 
 function App() {
+
+  // const [user, setUser] = useState(null);
+  // const [token, setToken] = useState(null);
+
+  const [tabs, setTabs] = useState(null)
+
+  useEffect(() => {
+    axios.get()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <TabsContext.Provider value={tabs}>
+      <div className="layout-container">
+        <div className="left-wrapper">
         
-      </header>
-    </div>
-  );
+        </div>
+        <div className="right-wrapper">
+          <div className="header-wrapper">
+            <Header />
+          </div>
+          <div className="content-wrapper">
+            test
+          </div>
+        </div>
+      </div>
+    </TabsContext.Provider>
+  )
 }
 
 export default App;
