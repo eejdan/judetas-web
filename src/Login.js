fd152060eebc95292b19f0c3d14f2a42579bcf47
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-13 23:24:17 +0300

    updated for new layout

--- src/Login.js
+++ src/Login.js
@@ -1,74 +1,97 @@
 
 import React, { useEffect, useState } from "react";
-import useAuth from "./hooks/useAuth";
-import useErrorQueue from "./hooks/useErrorQueue";
 import './Login.css'
+import styles from "./Login.module.css"
+import axios from 'axios';
 
-const axios = require('axios').default;
+export function LoginForm(props) {
 
-function LoginForm(props) {
-    const { auth, setAuth } = useAuth();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const { setError } = useErrorQueue();
-    const [success, setSuccess] = useState("")
-    const [successDisplay, setSuccessDisplay] = useState("none")
-    const handleSubmit = event => {
-        event.preventDefault()
-        axios.post('/api/auth/login', {
-            password: password, 
-            username: username
+    const [showSuccess, setShowSuccess] = useState(false);
+
+    const [showError, setShowError] = useState(false);
+    const [error, setError] = useState("");
+    var hideErrorTimeout;
+    const setErrorTimeout = (seconds) => {
+        clearTimeout(hideErrorTimeout);
+        hideErrorTimeout = setTimeout(
+            () => setShowError(false), seconds*1000)
+    }
+
+    useEffect(() => { // done
+        return () => { //pentru cleanup la demontarea componentei
+            clearTimeout(hideErrorTimeout);
+            setUsername('');
+            setPassword('');
+            setError('');
+        }
+    }, [])
+
+
+    const handleSubmit = event => { //done
+        event.preventDefault();
+
+        const statusResolvers = {
+            '200': (data) => {
+                setShowError(false)
+                setShowSuccess(true);
+                props.onLogin({
+                    session_id: data.session_id,
+                    username: username
+                });
+            }, 
+            '401': () => {
+                setError('Date incorecte.');
+                setErrorTimeout(5);
+            },
+            '500': () => {
+                setError('Eroare de server. Incercati mai tarziu')
+                setShowError(true);
+                setErrorTimeout(30);
+            }
+        }
+        axios.post('http://localhost:3030/api/auth/admin/login',{
+            username: username,
+            password: password
+        }, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            validateStatus: status => {
+                return !!(statusResolvers[status.toString()]);
+            }
         }).then( res => {
-            //temporar
-            setAuth({
-                loggedIn: true,
-                user: username,
-                session: res.data.sid,
-                authorized: false,
-                currentAccessToken: null
-            })
-            setSuccess("Date corecte!")
-            setSuccessDisplay("block")
-            setTimeout(() => {
-                setSuccess("");
-                setSuccessDisplay("none")
-            }, 15000)
-            props.onLogin();
+            statusResolvers[res.status](res.data);
         }).catch(err => {
             if(err.response) {
-                switch(err.response.status) {
-                    case 400:
-                        setError("Date de autentificare malformate.")
-                        break;
-                    case 401:
-                        setError("Nume de utilizator sau parola gresite.")
-                        break;
-                    case 404:
-                        setError("Server indisponibil")
-                        break;
-                    default:
-                        setError("Eroare");
-                        break;
-                }
+                setErrorTimeout(30);
+                setError('Eroare necunoscuta. Incercati mai tarziu');
+                setShowError(true);
             } else if (err.request) {
-                
+                setErrorTimeout(30);
+                setError('Server indisponibil. Incercati mai tarziu');
+                setShowError(true);
             } else {
                 console.log('err', err.message);
             }
             console.log(err.config);
-        }).then(() =>{
-
-        })
+        });
     }
     return(
         <React.Fragment>
             <form className="login-form" 
                 onSubmit={handleSubmit}>
-                <div 
-                    className="login-form-success" 
-                    style={{display: successDisplay}}>
-                        {success}
-                </div>
+                { showSuccess && (
+                    <div className="login-form-success" >
+                            Autentificat cu success. Va rugam asteptati.
+                    </div>
+                )}
+                { showError && (
+                    <div className={styles['error']}>
+                        { error }
+                    </div>
+                )}
                 <label>Nume de utilizator</label>
                 <input type={"text"}
                     onChange={e => setUsername(e.target.value)}
@@ -85,22 +108,9 @@ function LoginForm(props) {
 
 export default function Login(props) {
 
-
-    useEffect(() => {
-
-    }, [])
-
     return(
     <div className="login-box">
         <div className="top-span">&nbsp;</div>
-        <header className="header">
-            <div className="logo-box">
-                <span className="temp-logo-span"></span>
-            </div>
-            <div className="title-box">
-                JudetAs
-            </div>
-        </header>
         <div className="login-form-wrapper">       
             <LoginForm onLogin={props.onLogin}/>
         </div>
@@ -108,4 +118,3 @@ export default function Login(props) {
     </div>
     )
 }
-
