f3f492a9a4249c35cf38cddc2fa7970762bf3aee
eejdan <65033345+eejdan@users.noreply.github.com>
2022-05-13 23:23:51 +0300

    planned removal of providers

--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ import './index.css';
 import './colors.css'
 
 import { CookiesProvider } from 'react-cookie';
-
-import { AuthProvider } from './context/AuthProvider';
+import { ErrorProvider } from './context/ErrorProvider';
+import { ThemeProvider } from './context/ThemeProvider';
 
 import { createRoot } from 'react-dom/client';
-import { ErrorProvider } from './context/ErrorProvider';
 const container = document.getElementById('root')
 const root = createRoot(container);
 root.render(
-    <AuthProvider>
+    <ThemeProvider>
         <ErrorProvider>
             <CookiesProvider>
                 <App />
             </CookiesProvider>
         </ErrorProvider>
-    </AuthProvider>
+    </ThemeProvider>
 )
